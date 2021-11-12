05b1ff35f7379c3206a4895f60f77e86fd82ea82
coevpen <koinuevins@gmail.com>
2021-11-11 21:09:30 -0600

    add shop-input by numeric feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -215,33 +215,33 @@ var fight = function(enemy) {
 var shop = function(){
   //ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
-    // switch statement carries out choice
-    switch(shopOptionPrompt){
-      case "REFILL":
-      case "refill":
-        playerInfo.refillHealth();
-        break;
+  // parses the shopOptionPrompt to an int
+  shopOptionPrompt = parseInt(shopOptionPrompt);
 
-      case "UPGRADE":
-      case "upgrade":
-        playerInfo.upgradeAttack();
-        break;
+  // switch statement carries out choice
+  switch(shopOptionPrompt){
+    case 1:
+      playerInfo.refillHealth();
+      break;
 
-      case "LEAVE":
-      case "leave":
-        window.alert("Leaving the store.");
-        //do nothing, so function will end
-        break
-      default:
-        window.alert("You did not pick a valid option. Try again.");
+    case 2:
+      playerInfo.upgradeAttack();
+      break;
 
-        //call shop() again to force pllayer to pick a valid option
-        shop();
-        break;
-    }
+    case 3:
+      window.alert("Leaving the store.");
+      //do nothing, so function will end
+      break
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+
+      //call shop() again to force pllayer to pick a valid option
+      shop();
+      break;
+  }
 
 };
 
